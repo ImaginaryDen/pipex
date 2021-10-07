25726744223c9f54da95c50082ed98cd0a246026
Tomard Jamis <tjamis@su-b2.msk.21-school.ru>
2021-10-07 22:00:22 +0300

    Для 2 аргументов сделал

--- pipex.c
+++ pipex.c
@@ -37,64 +37,107 @@ int init_cmds(t_pipe_data *cmd, int size, char **argv, char **envp)
 	return (0);
 }
 
-int insert_pipe(t_pipe_data *cmds, int size, int file_in, int file_out)
+int *insert_pipe(t_pipe_data *cmds, int size, int file_in, int file_out)
 {
 	int	*end;
 	int	i;
 
 	i = 0;
-	end = malloc(sizeof(int) * size * 2);
-	while (i < size)
+	end = malloc(sizeof(int) * (size - 1) * 2);
+	if (!end)
+		return (NULL);
+	while (i < size - 1)
 	{
-		pipe(end + i * 2);
-		cmds[i].cmd_ard[1] = (end + i * 2)[0];
-		cmds[i + 1].cmd_ard[0] = (end + i * 2)[1];
+		if (pipe(end + i * 2))
+		{
+			free(end);
+			return (NULL);
+		}
+		cmds[i].fd_in_out[1] = end[i * 2 + 1];
+		cmds[i + 1].fd_in_out[0] = end[i * 2];
 		i++;
 	}
-	cmds[0].cmd_ard[0] = file_in;
+	cmds[0].fd_in_out[0] = file_in;
+	cmds[size - 1].fd_in_out[1] = file_out;
+	return (end);
+}
 
-	free(end);
-} 
+int  run_cmds(t_pipe_data *cmds, int *end, int size)
+{
+	int		i;
+	int		pid;
+	pid_t	*pid_cmd;
+	int		status;
+
+	pid_cmd = malloc(sizeof(pid_t) * size);
+	i = 0;
+	while (i < size)
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			free(pid_cmd);
+			perror("fork");
+			return (1);
+		}
+		if (!pid)
+		{
+			close(end[i]);
+			ft_cmd(cmds + i);
+			return (1);
+		}
+		pid_cmd[i] = pid;
+		i++;
+	}
+	i = 0;
+	while (i < (size - 1) * 2)
+	{
+		close(end[i]);
+		i++;
+	}
+	i = 0;
+	while (i < size)
+	{
+		waitpid(pid_cmd[i], &status, 0);
+		i++;
+	}
+	free(pid_cmd);
+	return (0);
+}
+
+void free_cmds(t_pipe_data *cmds, int size)
+{
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		free_cmd(cmds + i);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_pipe_data	cmds[2];
+	t_pipe_data	*cmds;
 	int			file_in;
 	int			file_out;
-	pid_t		brench_1;
-	pid_t		brench_2;
-	int			end[2];
 	int			status;
+	int			*end;
 
-	if (argc != 5 || files_open(argv[1], argv[4], &file_in, &file_out)
-		|| pipe(end))
+	if (files_open(argv[1], argv[argc - 1], &file_in, &file_out))
 		return (1);
-	init_cmds(cmds, 2, argv, envp); 
-	cmds[0].fd_in_out[0] = file_in;
-	cmds[0].fd_in_out[1] = end[1];
-	cmds[1].fd_in_out[0] = end[0];
-	cmds[1].fd_in_out[1] = file_out;
-	brench_2 = fork();
-	if (!brench_2)
-	{
-		close(end[1]);
-		ft_cmd(&cmds[1]);
+	cmds = malloc(sizeof(t_pipe_data) * (argc - 3));
+	init_cmds(cmds, argc - 3, argv, envp); 
+	end = insert_pipe(cmds, argc - 3, file_in, file_out);
+	if (end == NULL)
+		return (1);
+	if (run_cmds(cmds, end, argc - 3) == 1)
 		return (0);
-	}
-	brench_1 = fork();
-	if (!brench_1)
-	{
-		close(end[0]);
-		ft_cmd(&cmds[0]);
-		return (0);
-	}
-	close(end[1]);
-	close(end[0]);
 	close(file_out);
 	close(file_in);
-	waitpid(brench_1, &status, 0);
-	waitpid(brench_2, &status, 0);
-	free_cmd(&cmds[1]);
-	free_cmd(&cmds[0]);
+	free(end);
+	free_cmds(cmds, argc - 3);
+	free(cmds);
 	return (0);
 }
